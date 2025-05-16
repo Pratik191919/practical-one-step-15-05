5aa64aec536251175e11a55875f52248e1a1da88
Pratik191919 <prartikparmar191919@gmail.com>
2025-05-16 15:19:31 +0530

    update build error

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextResponse } from 'next/server';
 import { tokenizeCard } from '@/lib/utils/tokenization';
 import { paymentFormSchema } from '@/lib/validation/schemas';
